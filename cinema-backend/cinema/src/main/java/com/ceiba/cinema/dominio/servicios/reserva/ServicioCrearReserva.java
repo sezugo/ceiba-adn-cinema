35f5bebca4c09ac424e61ababa92f72bba3e46c0
Sebastián Zuluaga Gómez - Ceiba Software <sebastian.zuluaga@ceiba.com.co>
2021-01-05 14:41:38 -0500

    Logica negocio + Test

--- cinema-backend/cinema/src/main/java/com/ceiba/cinema/dominio/servicios/reserva/ServicioCrearReserva.java
+++ cinema-backend/cinema/src/main/java/com/ceiba/cinema/dominio/servicios/reserva/ServicioCrearReserva.java
@@ -3,10 +3,14 @@ import com.ceiba.cinema.dominio.modelo.Reserva;
 import com.ceiba.cinema.dominio.puerto.repositorio.RepositorioReserva;
 import com.ceiba.cinema.dominio.puerto.repositorio.RepositorioCliente;
 import com.ceiba.cinema.dominio.puerto.repositorio.RepositorioPelicula;
+import com.ceiba.cinema.dominio.validar.ValidarLogicaNegocio;
+
+import java.time.LocalDateTime;
 
 public class ServicioCrearReserva {
 
     private RepositorioReserva repositorioReserva;
+    private ValidarLogicaNegocio validarLogicaNegocio = new ValidarLogicaNegocio();
 
 
     public ServicioCrearReserva(RepositorioReserva repositorioReserva){
@@ -15,8 +19,16 @@ public class ServicioCrearReserva {
     }
 
     public void ejecutar(Reserva reserva){
+        LocalDateTime fechaActual = LocalDateTime.now();
+        validarLogicaNegocio.diaDelMesSinServicio(fechaActual);
+        LocalDateTime fechaEntrega = validarLogicaNegocio.calcularFechaEntrega();
+        reserva.setFecha(fechaEntrega);
+        Double valor = validarLogicaNegocio.calcularValorTotal(reserva.getFecha(), reserva.getPelicula().getValor());
+        reserva.setValorTotal(valor);
+
 
         //VALIDAR LOGICA NEGOCIO
+
         this.repositorioReserva.crearReserva(reserva);
 
     }
